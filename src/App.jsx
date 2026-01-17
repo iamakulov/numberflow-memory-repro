614fe11ff40daa41a3b0270b36480032bf2a927a
Ivan Akulov <iamakulov@outlook.com>
2026-01-17 16:52:03 +0000

    Update for CodeSandbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import NumberFlow, { NumberFlowGroup } from '@number-flow/react'
-import { useState, useEffect } from 'react'
+import NumberFlow, { NumberFlowGroup } from "@number-flow/react";
+import { useState, useEffect } from "react";
 
 export function App() {
-  const [number, setNumber] = useState(100)
+  const [number, setNumber] = useState(100);
 
   useEffect(() => {
-    const socket = new WebSocket('ws://localhost:7071')
+    const socket = new WebSocket("ws://localhost:7071");
 
     socket.onmessage = (event) => {
-      setNumber(Number(event.data))
-    }
+      setNumber(Number(event.data));
+    };
 
     socket.onerror = () => {
-      alert(`WebSocket error: ${socket.error}`)
-    }
+      alert(`WebSocket error: ${socket.error}`);
+    };
 
-    return () => socket.close()
-  }, [])
+    return () => socket.close();
+  }, []);
 
   return (
     <div
       style={{
-        fontFamily: 'ui-sans-serif, system-ui, sans-serif',
+        fontFamily: "ui-sans-serif, system-ui, sans-serif",
         padding: 32,
-        display: 'flex',
-        flexDirection: 'column',
+        display: "flex",
+        flexDirection: "column",
         gap: 16,
       }}
     >
@@ -36,20 +36,28 @@ export function App() {
           transformTiming={{
             duration: 450,
             easing:
-              'linear(0,.005,.019,.039,.066,.096,.129,.165,.202,.24,.278,.316,.354,.39,.426,.461,.494,.526,.557,.586,.614,.64,.665,.689,.711,.731,.751,.769,.786,.802,.817,.831,.844,.856,.867,.877,.887,.896,.904,.912,.919,.925,.931,.937,.942,.947,.951,.955,.959,.962,.965,.968,.971,.973,.976,.978,.98,.981,.983,.984,.986,.987,.988,.989,.99,.991,.992,.992,.993,.994,.994,.995,.995,.996,.996,.9963,.9967,.9969,.9972,.9975,.9977,.9979,.9981,.9982,.9984,.9985,.9987,.9988,.9989,1)',
+              "linear(0,.005,.019,.039,.066,.096,.129,.165,.202,.24,.278,.316,.354,.39,.426,.461,.494,.526,.557,.586,.614,.64,.665,.689,.711,.731,.751,.769,.786,.802,.817,.831,.844,.856,.867,.877,.887,.896,.904,.912,.919,.925,.931,.937,.942,.947,.951,.955,.959,.962,.965,.968,.971,.973,.976,.978,.98,.981,.983,.984,.986,.987,.988,.989,.99,.991,.992,.992,.993,.994,.994,.995,.995,.996,.996,.9963,.9967,.9969,.9972,.9975,.9977,.9979,.9981,.9982,.9984,.9985,.9987,.9988,.9989,1)",
           }}
         />
       </NumberFlowGroup>
       <div>
         Steps:
         <ol>
-          <li>Run the websocket server: <code>npm run server</code></li>
-          <li>Run the app: <code>npm install</code> then <code>npm run dev</code></li>
+          <li>
+            Run the websocket server: <code>npm run server</code> (autoruns in
+            CodeSandbox)
+          </li>
+          <li>
+            Run the app: <code>npm install</code> then <code>npm run dev</code>{" "}
+            (autoruns in CodeSandbox)
+          </li>
           <li>Open the page. Open the DevTools in a separate window.</li>
           <li>Background the tab for 5-10 minutes.</li>
-          <li>Observe the JS heap size in DevTools while the tab is backgrounded</li>
+          <li>
+            Observe the JS heap size in DevTools while the tab is backgrounded
+          </li>
         </ol>
       </div>
     </div>
-  )
+  );
 }
